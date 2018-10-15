2e440fb886539f5e9c9055887f1a57752915ddc2
Sachin CV <beckhamd36@gmail.com>
2018-10-15 21:06:14 +0530

    api built

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Resources\Article as ArticleResource;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get articles 
+        $articles = Article::paginate(15);
+
+        //return collection of articles as resource
+        return ArticleResource::collection($articles);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = $request->isMethod(put)? Article::findOrFail($request->article_id):new Article;
+
+        $article->id = $request->input('article_id');
+        $article->title = $request->input('title');
+        $article->body = $request->input('body');
+
+        if($article->save()){
+            return new ArticleResource($article);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //get single article
+        $article = Article::findOrFail($id);
+        //return as resource 
+        return new ArticleResource($article);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //get single article
+        $article = Article::findOrFail($id);
+        if($article->delete()){
+            return new ArticleResource($article);
+        }
+        
+    }
+}
